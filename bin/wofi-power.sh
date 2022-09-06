9f115c3022e366ac826ea2ccd77b3e02f9815aba
Juri Sacchetta <jurisacchetta@gmail.com>
2022-09-06 11:24:36 +0200

    Fixed logout function

--- bin/wofi-power.sh
+++ bin/wofi-power.sh
@@ -6,7 +6,7 @@ selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /
 
 case $selected in
   logout)
-    swaymsg exit;;
+    hyprctl dispatch exit now;;
   suspend)
     exec systemctl suspend;;
   reboot)
