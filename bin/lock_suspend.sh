9e16958999b104a1a5608ad0dec4576dc3d5e47b
Juri Sacchetta <jurisacchetta@gmail.com>
2022-09-17 17:36:07 +0200

    Some config updated

--- bin/lock_suspend.sh
+++ bin/lock_suspend.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-swayidle -w timeout 300 'systemctl --user start lock.service' timeout 450 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 600 'systemctl suspend' before-sleep 'systemctl --user start lock.service'
+swayidle -w timeout 120 'systemctl --user start lock.service' timeout 180 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 240 'systemctl suspend' resume 'hyprctl dispatch dpms on' \before-sleep 'systemctl --user start lock.service'
